7538be2cebcd5f9bfa8bf0d6b8af5cf398c86e92
SURAJIT MAJI <132469890+SurajitMaji@users.noreply.github.com>
2025-10-11 01:06:22 +0530

    Add files via upload

---
+++ script/designStudio.js
@@ -0,0 +1,28 @@
+
+
+// main dress image zoom control 
+
+let main_image = document.getElementById('main_dress_image');
+let currentZoom = 1;  // initial zoom level
+let maxZoom = 3; // maximum Zoom level
+console.log('erdafa');
+function mainDressZoomIn() {
+
+  if (currentZoom < maxZoom) {
+    currentZoom += 0.2;
+    main_image.style.transform = `scale(${currentZoom})`;
+    main_image.style.cursor = 'zoom-in';
+  }
+}
+
+function mainDressZoomOut(){
+
+    if(currentZoom>1){      
+        currentZoom -= 0.2;
+        main_image.style.transform = `scale(${currentZoom})`;
+        main_image.style.cursor = 'zoom-in';
+    }
+}
+
+
+
